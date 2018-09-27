d1e8211526814024f29c0bb822aa02e819d6fadb
Erlon Júnior <17034932+ejrgeek@users.noreply.github.com>
2018-09-27 01:55:01 -0300

    Add files via upload

---
+++ Exportado (HTML5)/offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1511470576,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/cell-sheet0.png",
+		"images/tiledbackground.png",
+		"images/tiledbackground2.png",
+		"images/bottompipe-sheet0.png",
+		"images/toppipe-sheet0.png",
+		"images/fundo-sheet0.png",
+		"images/menu-sheet0.png",
+		"images/playicon-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
